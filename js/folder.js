f5b5ac1a24badee74fc24fc320a9808ad86e08b7
Ikeh Akinyemi <mrikehchukwuka@gmail.com>
2021-01-12 23:45:33 +0100

    UI feature: worked on the UX of clicking on the foldericon to highlight the folder's name text

--- js/folder.js
+++ js/folder.js
@@ -2,11 +2,12 @@
 
 let folderContainer = document.querySelector("#folder-section"),
     folders = document.querySelectorAll(".folder__folder-container"),
+    folderNames = document.querySelectorAll(".folderName");
     bodyUI = document.querySelector(".main");
 
 body.addEventListener("click", (e) => {
   if (folderContainer.className === "folder") {
-    folders.forEach(folder => {
+    folders.forEach( folder => {
       folder.id = "grid-list-view";
     });
   };
@@ -16,4 +17,23 @@ body.addEventListener("click", (e) => {
       folder.id = "";
     });
   }
-})
+});
+
+folders.forEach( folder => {
+  folder.addEventListener("click", (e) => {
+    if ( !folderContainer.className.includes("folder--list-view") && 
+    (e.target.className == "folder-name-span" || e.target.className == "folderIcon" ||e.target.className.includes("fa-folder"))) {
+      folderNames.forEach( folderName => {
+        folderName.style.backgroundColor = "unset";
+        folderName.style.color = "unset";
+        folderName.firstElementChild.style.wordBreak = "unset";
+      });
+
+      e.target.closest(".folder__folder-container").lastElementChild.style.backgroundColor = "#e05134";
+      e.target.closest(".folder__folder-container").lastElementChild.firstElementChild.style.wordBreak = "break-all";
+      e.target.closest(".folder__folder-container").lastElementChild.style.borderRadius = "3px";
+      e.target.closest(".folder__folder-container").lastElementChild.style.color = "#ffffff";
+    }
+  })
+});
+
